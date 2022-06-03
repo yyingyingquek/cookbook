8e82c365c770e0027c04558179961f35f2d284e6
yingyingquek <quekyingying26@gmail.com>
2022-06-03 12:19:14 +0800

    dataStorageService: fetchRecipe & saveRecipe

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://coobook-max-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe((response) => console.log(response));
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(
+        'https://coobook-max-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipes) => {
+        console.log(recipes);
+        this.recipeService.setRecipes(recipes);
+      });
+  }
+}
